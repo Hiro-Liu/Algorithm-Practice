0011a862104e609bd97faa8320185a514c78faeb
Liulei <57137317+Hiro-Liu@users.noreply.github.com>
2020-03-20 21:01:04 +0800

    practice for section 2.5

--- 有序插入.c
+++ 有序插入.c
@@ -1,25 +1,26 @@
 #include <stdio.h>
+#define N 10
 
 int main()
 {
-	int a[10];
+	int a[N];
 	int x;
-	for (int i = 0; i < 9; i++)
+	for (int i = 0; i < N -1; i++)
 		scanf("%d", &a[i]);
 	scanf("%d", &x);
-	for (int i = 0; i < 9 ; i++)
+	for (int i = 0; i < N - 1; i++)
 	{
 		if (x < a[i])
 		{
-			for (int j = 9; j > i; j--)
+			for (int j = N -1 ; j > i; j--)
 			{
 				a[j] = a[j - 1];
 			}
-			a[i] = x;	
+			a[i] = x;
 			break;
 		}
 	}
-	for (int i = 0; i < 10; i++)
+	for (int i = 0; i < N; i++)
 		printf("%d ", a[i]);
 	return 0;
 }
