3862ef83162a0510b687608eb70a173201586d20
Liulei <57137317+Hiro-Liu@users.noreply.github.com>
2020-03-24 21:58:29 +0800

    practice for section 2.8
    编写两个函数input和print，分别用来输入N个学生的数据记录和打印这N个学生的记录。对于每一个学生，其记录包含了学号、名字、M门课程的成绩共M+2项。用主函数分别调用input和print函数进行输入和输出。要求使用结构体数组实现，结构体中包括了每个学生的M+2项记录。
    
    输入：共有5行，每行包含了一个学生的学号（整数）、名字（长度不超过19的无空格字符串）和3门课程的成绩（0至100之间的整数），用空格隔开。
    
    输出：与输入格式相同，每行输出一个学生的所有记录。请注意行尾输出换行。

--- [Section 2.8] 结构体/储存学生信息(结构体+函数).c
+++ [Section 2.8] 结构体/储存学生信息(结构体+函数).c
@@ -1,29 +1,29 @@
 #include <stdio.h>
-#define N 5    //共N个学生的信息
-#define S 3    //每个学生需存S门成绩
+#define N 5    //学生数
+#define M 3    //科目数
 
 struct studentInfo{
 	int number;
 	char name[20];
-	int score[S];
+	int score[M];
 }stu[N];
 
-void input(struct studentInfo stu[])
+void input()
 {
 	for (int i = 0; i < N; i++)
 	{
 		scanf("%d %s", &stu[i].number, &stu[i].name);
-		for (int j = 0; j < S; j++)
+		for (int j = 0; j < M; j++)
 			scanf("%d", &stu[i].score[j]);
 	}
 }
 
-void print(struct studentInfo stu[])
+void print()
 {
 	for (int i = 0; i < N; i++)
 	{
 		printf("%d %s", stu[i].number, stu[i].name);
-		for (int j = 0; j < S; j++)
+		for (int j = 0; j < M; j++)
 			printf(" %d", stu[i].score[j]);
       
 		printf("\n");
