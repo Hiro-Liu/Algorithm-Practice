a528c580138980d6182d283e8c4231a1a20f70a8
Liulei <57137317+Hiro-Liu@users.noreply.github.com>
2020-04-08 09:38:39 +0800

    practice for section 3.4
    输入四个数表示 ：年、月、日和累加天数。输出天数累加后的日期，格式YYYY-MM-DD

---
+++ [Chapter 3] 入门模拟/[Section 3.4] 日期处理/它的若干天后是几月几号？.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int month[13][2] = {{0, 0},   {31, 31}, {28, 29}, {31, 31}, {30, 30},
+                    {31, 31}, {30, 30}, {31, 31}, {31, 31}, {30, 30},
+                    {31, 31}, {30, 30}, {31, 31}};  //平年和闰年每个月的天数
+
+bool isLeap(int year) {  //判断是否是闰年
+    return (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0);
+}
+
+int main() {
+    int m;
+    scanf("%d", &m);
+    while (m--) {
+        int y, m, d, delay;
+        scanf("%d %d %d %d", &y, &m, &d, &delay);
+        while (delay--) {
+            d++;
+            if (d == month[m][isLeap(y)] + 1) {
+                d = 1;
+                m++;
+            }
+            if (m == 13) {
+                m = 1;
+                y++;
+            }
+        }
+        printf("%04d-%02d-%02d\n", y, m, d);
+    }
+    return 0;
+}
